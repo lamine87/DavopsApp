e66ad7f5b12686661e2fa254f68bb7be9bdbfd09
lamine87 <laminenatidiarra@yahoo.fr>
2024-01-11 10:49:13 +0100

    Update MyVar Workflow

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ accuracy = accuracy_score(y_test, y_pred)
 print(f"Précision du modèle : {accuracy:.2f}")
 
 # Définir un seuil de classification
-seuil_classification = float(os.environ.get('MYVAR'))
+seuil_classification = float(os.environ["MYVAR"])
 
 # Vérifier si le seuil est atteint
 if accuracy >= seuil_classification:
