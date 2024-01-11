2209a4399bc6be78cfbb1f46569371ac7a7ececc
lamine87 <laminenatidiarra@yahoo.fr>
2024-01-11 10:21:19 +0100

    Environnement

--- test.py
+++ test.py
@@ -1,28 +1,29 @@
+import os
 import pandas as pd
 import pickle
 from sklearn.metrics import accuracy_score
 
 # Charger le modèle
-with open('cancer_model.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
 
 # Charger ou préparer les données de test
-url = "https://raw.githubusercontent.com/AbdallahTayeb/DevOps-Course/main/sample.csv"
-test_data = pd.read_csv(url)
-
-# Assumer que les colonnes sont les mêmes que dans les données d'entraînement
-X_test = test_data[model]
+df = pd.read_csv('sample.csv',sep=';')
+X_test = df.drop('target', axis=1)
+y_test = df['target']
 
 # Faire des prédictions
 y_pred = model.predict(X_test)
 
 # Calculer la précision
-accuracy = accuracy_score(test_data['target'], y_pred)
-print(f"Accuracy on test data: {accuracy}")
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Précision du modèle : {accuracy:.2f}")
 
-# Définir un seuil de classification (vous pouvez le personnaliser selon vos besoins)
-threshold = 0.5
+# Définir un seuil de classification
+seuil_classification = float(os.environ.get('MyVAR'))
 
 # Vérifier si le seuil est atteint
-predictions_above_threshold = (model.predict_proba(X_test)[:, 1] > threshold).astype(int)
-print(f"Predictions above threshold: {predictions_above_threshold}")
+if accuracy >= seuil_classification:
+    print("Le seuil de classification est atteint. Le modèle est prêt pour le déploiement.")
+else:
+    print("Le seuil de classification n'est pas atteint. Le modèle nécessite une amélioration.")
